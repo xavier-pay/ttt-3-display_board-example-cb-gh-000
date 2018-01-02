99d4c57d8cc5f8f4e47cdb0b28f85925556e83c9
Aurelien Xavier  Payong D <xavier.payong@gmail.com>
2018-01-02 04:22:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,13 @@
-def draw_line
+def draw_cell
   puts "   "+"|"+"   "+"|"+"   "+"|"
+end
+def draw_line
+  puts "-----------"
 end
 
 def display_board
   draw_line
+  
   draw_line
   draw_line
 end
